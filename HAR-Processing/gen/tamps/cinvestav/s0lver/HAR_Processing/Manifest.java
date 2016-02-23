08c33b08064d0a07c429431603469913c1dd843c
s0lver <rperez.github@gmail.com>
2016-02-23 16:51:44 -0600

    HotFix files of modules

---
+++ HAR-Processing/gen/tamps/cinvestav/s0lver/HAR_Processing/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package tamps.cinvestav.s0lver.HAR_Processing;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
